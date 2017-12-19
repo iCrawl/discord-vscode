115729369a9eb1b199db0b73455423497525f1de
pyrox18 <haryzizzudin@gmail.com>
2017-12-20 00:09:26 +0800

    Add workspace elapsed time feature

--- src/extension.ts
+++ src/extension.ts
@@ -93,12 +93,18 @@ function initRPC(clientID: string): void {
 		setActivity();
 		// Set the activity once on ready
 		setTimeout(() => rpc.setActivity(activity), 500);
-		eventHandlers.add(workspace.onDidChangeTextDocument(() => setActivity()))
-			.add(workspace.onDidOpenTextDocument(() => setActivity()))
-			.add(workspace.onDidCloseTextDocument(() => setActivity()))
-			.add(debug.onDidChangeActiveDebugSession(() => setActivity()))
+		eventHandlers.add(debug.onDidChangeActiveDebugSession(() => setActivity()))
 			.add(debug.onDidStartDebugSession(() => setActivity()))
 			.add(debug.onDidTerminateDebugSession(() => setActivity()));
+		if (config.get('workspaceElapsedTime')) {
+			eventHandlers.add(workspace.onDidChangeTextDocument(() => setActivity(true)))
+				.add(workspace.onDidOpenTextDocument(() => setActivity(true)))
+				.add(workspace.onDidCloseTextDocument(() => setActivity(true)));
+		} else {
+			eventHandlers.add(workspace.onDidChangeTextDocument(() => setActivity()))
+				.add(workspace.onDidOpenTextDocument(() => setActivity()))
+				.add(workspace.onDidCloseTextDocument(() => setActivity()));
+		}
 		// Make sure to listen to the close event and dispose and destroy everything accordingly.
 		rpc.transport.once('close', () => {
 			if (!config.get('enabled')) return;
@@ -151,7 +157,7 @@ function destroyRPC(): void {
 }
 
 // This function updates the activity (The Client's Rich Presence status).
-function setActivity(): void {
+function setActivity(workspaceElapsedTime: boolean = false): void {
 	// Do not continue if RPC isn't initalized.
 	if (!rpc) return;
 	if (window.activeTextEditor && window.activeTextEditor.document.fileName === lastKnownFileName) return;
@@ -168,11 +174,14 @@ function setActivity(): void {
 		})]
 		: 'vscode-big';
 
+	// Get the previous activity start timestamp (if available) to preserve workspace elapsed time
+	let previousTimestamp = null;
+	if (activity) previousTimestamp = activity['startTimestamp'];
 	// Create a JSON Object with the user's activity information.
 	activity = {
 		details: generateDetails('detailsDebugging', 'detailsEditing', 'detailsIdle'),
 		state: generateDetails('lowerDetailsDebugging', 'lowerDetailsEditing', 'lowerDetailsIdle'),
-		startTimestamp: new Date().getTime() / 1000,
+		startTimestamp: !previousTimestamp && !workspaceElapsedTime ? new Date().getTime() / 1000 : previousTimestamp,
 		largeImageKey: largeImageKey
 			? largeImageKey.image
 				|| largeImageKey
