ced4c6012cdaa2a6b30c9066253aebfa5dd570d4
Crawl <icrawltogo@gmail.com>
2018-11-07 03:28:36 +0100

    feat: add constants for live share

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const LIVE_SHARE_BASE_URL = 'insiders.liveshare.vsengsaas.visualstudio.com';
+export const VSLS_EXTENSION_ID = 'ms-vsliveshare.vsliveshare';
+
+export const LIVE_SHARE_COMMANDS = {
+	START: 'liveshare.start',
+	END: 'liveshare.end',
+	JOIN: 'liveshare.join'
+}
