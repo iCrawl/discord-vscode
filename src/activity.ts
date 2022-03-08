3f8472e4232ae754213f6d1e0fba39cd16bf4e51
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-03-08 12:53:43 +0700

    Optimization

--- src/activity.ts
+++ src/activity.ts
@@ -1,4 +1,4 @@
-import { basename, parse, sep } from 'path';
+import { parse, sep } from 'path';
 import { debug, env, Selection, TextDocument, window, workspace } from 'vscode';
 import GitUrlParse from 'git-url-parse';
 
@@ -42,50 +42,24 @@ async function repoDetails(_raw: string) {
 
 	const git = await getGit();
 	const repo = git?.repositories.find((repo) => repo.ui.selected);
+	const repo_data = repo?.state.remotes[0].fetchUrl ? GitUrlParse(repo.state.remotes[0].fetchUrl) : undefined;
 
-	if (raw.includes(REPLACE_KEYS.GitBranch)) {
-		if (repo) {
-			raw = raw.replace(REPLACE_KEYS.GitBranch, repo.state.HEAD?.name ?? FAKE_EMPTY);
-		} else {
-			raw = raw.replace(REPLACE_KEYS.GitBranch, UNKNOWN_GIT_BRANCH);
-		}
-	}
-
-	if (raw.includes(REPLACE_KEYS.GitRepoName)) {
-		const repo_data = repo?.state.remotes[0].fetchUrl ? GitUrlParse(repo.state.remotes[0].fetchUrl) : undefined;
-		if (repo_data) {
-			raw = raw.replace(REPLACE_KEYS.GitRepoName, repo_data.name);
-		} else {
-			raw = raw.replace(REPLACE_KEYS.GitRepoName, UNKNOWN_GIT_REPO_NAME);
-		}
-	}
-
-	if (raw.includes(REPLACE_KEYS.GitRepoOwner)) {
-		const repo_data = repo?.state.remotes[0].fetchUrl ? GitUrlParse(repo.state.remotes[0].fetchUrl) : undefined;
-		if (repo_data) {
-			raw = raw.replace(REPLACE_KEYS.GitRepoOwner, repo_data.owner);
-		} else {
-			raw = raw.replace(REPLACE_KEYS.GitRepoOwner, UNKNOWN_GIT_REPO_OWNER);
-		}
-	}
+	raw = raw
+		.replace(REPLACE_KEYS.GitBranch, repo ? repo.state.HEAD?.name ?? FAKE_EMPTY : UNKNOWN_GIT_BRANCH)
+		.replace(REPLACE_KEYS.GitRepoName, repo_data?.name ?? UNKNOWN_GIT_REPO_NAME)
+		.replace(REPLACE_KEYS.GitRepoOwner, repo_data?.owner ?? UNKNOWN_GIT_REPO_OWNER);
 
 	return raw;
 }
 
-async function fileDetails(_raw: string, document: TextDocument, selection: Selection) {
+async function fileDetails(_raw: string, document: TextDocument) {
 	let raw = _raw.slice();
 
-	if (raw.includes(REPLACE_KEYS.TotalLines)) {
-		raw = raw.replace(REPLACE_KEYS.TotalLines, document.lineCount.toLocaleString());
-	}
+	const { dir, base: fileName } = parse(document.fileName);
+	const split = dir.split(sep);
+	const dirName = split[split.length - 1];
 
-	if (raw.includes(REPLACE_KEYS.CurrentLine)) {
-		raw = raw.replace(REPLACE_KEYS.CurrentLine, (selection.active.line + 1).toLocaleString());
-	}
-
-	if (raw.includes(REPLACE_KEYS.CurrentColumn)) {
-		raw = raw.replace(REPLACE_KEYS.CurrentColumn, (selection.active.character + 1).toLocaleString());
-	}
+	const fileIcon = resolveFileIcon(document);
 
 	if (raw.includes(REPLACE_KEYS.FileSize)) {
 		let currentDivision = 0;
@@ -95,72 +69,87 @@ async function fileDetails(_raw: string, document: TextDocument, selection: Sele
 		} catch {
 			size = document.getText().length;
 		}
-		const originalSize = size;
-		if (originalSize > 1000) {
-			size /= 1000;
+		while (size > 1000) {
 			currentDivision++;
-			while (size > 1000) {
-				currentDivision++;
-				size /= 1000;
-			}
+			size /= 1000;
 		}
 
-		raw = raw.replace(
-			REPLACE_KEYS.FileSize,
-			`${originalSize > 1000 ? size.toFixed(2) : size}${FILE_SIZES[currentDivision]}`,
-		);
+		raw = raw.replace(REPLACE_KEYS.FileSize, `${size.toFixed(currentDivision ? 2 : 0)}${FILE_SIZES[currentDivision]}`);
 	}
 
-	return repoDetails(raw);
+	raw = raw
+		.replace(REPLACE_KEYS.DirName, dirName)
+		.replace(REPLACE_KEYS.FileName, fileName)
+		.replace(REPLACE_KEYS.LanguageLowerCase, toLower(fileIcon))
+		.replace(REPLACE_KEYS.LanguageTitleCase, toTitle(fileIcon))
+		.replace(REPLACE_KEYS.LanguageUpperCase, toUpper(fileIcon))
+		.replace(REPLACE_KEYS.TotalLines, document.lineCount.toLocaleString());
+
+	return raw;
+}
+
+async function editDetails(_raw: string, selection: Selection) {
+	let raw = _raw.slice();
+
+	if (raw.includes(REPLACE_KEYS.CurrentLine)) {
+		raw = raw.replace(REPLACE_KEYS.CurrentLine, (selection.active.line + 1).toLocaleString());
+	}
+
+	if (raw.includes(REPLACE_KEYS.CurrentColumn)) {
+		raw = raw.replace(REPLACE_KEYS.CurrentColumn, (selection.active.character + 1).toLocaleString());
+	}
+	raw = raw
+		.replace(REPLACE_KEYS.CurrentLine, (selection.active.line + 1).toLocaleString())
+		.replace(REPLACE_KEYS.CurrentColumn, (selection.active.character + 1).toLocaleString());
+
+	return raw;
+}
+
+async function workspaceDetails(_raw: string, document: TextDocument) {
+	let raw = _raw.slice();
+	const config = getConfig();
+	const noWorkspaceFound = config[CONFIG_KEYS.LowerDetailsNoWorkspaceFound].replace(REPLACE_KEYS.Empty, FAKE_EMPTY);
+	const workspaceFolder = workspace.getWorkspaceFolder(document.uri);
+	const workspaceFolderName = workspaceFolder?.name ?? noWorkspaceFound;
+	const workspaceName = workspace.name?.replace(REPLACE_KEYS.VSCodeWorkspace, EMPTY) ?? workspaceFolderName;
+	const workspaceAndFolder = `${workspaceName}${workspaceFolderName === FAKE_EMPTY ? '' : ` - ${workspaceFolderName}`}`;
+	if (workspaceFolder) {
+		const { name } = workspaceFolder;
+		const relativePath = workspace.asRelativePath(document.fileName).split(sep);
+		relativePath.splice(-1, 1);
+		raw = raw.replace(REPLACE_KEYS.FullDirName, `${name}${sep}${relativePath.join(sep)}`);
+	}
+	raw = raw
+		.replace(REPLACE_KEYS.Workspace, workspaceName)
+		.replace(REPLACE_KEYS.WorkspaceFolder, workspaceFolderName)
+		.replace(REPLACE_KEYS.WorkspaceAndFolder, workspaceAndFolder);
+	return raw;
+}
+
+async function catchDetailsErrors<T extends any[]>(
+	addDetails: (_raw: string, ...args: T) => Promise<string>,
+	detail_name: string,
+	...args: Parameters<typeof addDetails>
+) {
+	let raw = args[0].slice();
+	try {
+		raw = await addDetails.apply(undefined, [...args]);
+	} catch (error) {
+		log(LogLevel.Error, `Failed to generate ${detail_name} details: ${error as string}`);
+	}
+	return raw;
 }
 
 async function details(idling: CONFIG_KEYS, editing: CONFIG_KEYS, debugging: CONFIG_KEYS) {
 	const config = getConfig();
-	let raw = (config[idling] as string).replace(REPLACE_KEYS.Empty, FAKE_EMPTY);
 
+	let raw = config[window.activeTextEditor?idling:debug.activeDebugSession?debugging:editing] as string;
+	raw = raw.replace(REPLACE_KEYS.Empty, FAKE_EMPTY);
 	if (window.activeTextEditor) {
-		const fileName = basename(window.activeTextEditor.document.fileName);
-		const { dir } = parse(window.activeTextEditor.document.fileName);
-		const split = dir.split(sep);
-		const dirName = split[split.length - 1];
-
-		const noWorkspaceFound = config[CONFIG_KEYS.LowerDetailsNoWorkspaceFound].replace(REPLACE_KEYS.Empty, FAKE_EMPTY);
-		const workspaceFolder = workspace.getWorkspaceFolder(window.activeTextEditor.document.uri);
-		const workspaceFolderName = workspaceFolder?.name ?? noWorkspaceFound;
-		const workspaceName = workspace.name?.replace(REPLACE_KEYS.VSCodeWorkspace, EMPTY) ?? workspaceFolderName;
-		const workspaceAndFolder = `${workspaceName}${
-			workspaceFolderName === FAKE_EMPTY ? '' : ` - ${workspaceFolderName}`
-		}`;
-
-		const fileIcon = resolveFileIcon(window.activeTextEditor.document);
-
-		if (debug.activeDebugSession) {
-			raw = config[debugging] as string;
-		} else {
-			raw = config[editing] as string;
-		}
-
-		if (workspaceFolder) {
-			const { name } = workspaceFolder;
-			const relativePath = workspace.asRelativePath(window.activeTextEditor.document.fileName).split(sep);
-			relativePath.splice(-1, 1);
-			raw = raw.replace(REPLACE_KEYS.FullDirName, `${name}${sep}${relativePath.join(sep)}`);
-		}
-
-		try {
-			raw = await fileDetails(raw, window.activeTextEditor.document, window.activeTextEditor.selection);
-		} catch (error) {
-			log(LogLevel.Error, `Failed to generate file details: ${error as string}`);
-		}
-		raw = raw
-			.replace(REPLACE_KEYS.FileName, fileName)
-			.replace(REPLACE_KEYS.DirName, dirName)
-			.replace(REPLACE_KEYS.Workspace, workspaceName)
-			.replace(REPLACE_KEYS.WorkspaceFolder, workspaceFolderName)
-			.replace(REPLACE_KEYS.WorkspaceAndFolder, workspaceAndFolder)
-			.replace(REPLACE_KEYS.LanguageLowerCase, toLower(fileIcon))
-			.replace(REPLACE_KEYS.LanguageTitleCase, toTitle(fileIcon))
-			.replace(REPLACE_KEYS.LanguageUpperCase, toUpper(fileIcon));
+		raw = await catchDetailsErrors(editDetails, 'edit', raw, window.activeTextEditor.selection);
+		raw = await catchDetailsErrors(fileDetails, 'file', raw, window.activeTextEditor.document);
+		raw = await catchDetailsErrors(workspaceDetails, 'workspace', raw, window.activeTextEditor.document);
+		raw = await catchDetailsErrors(repoDetails, 'repository', raw);
 	}
 
 	return raw;
@@ -171,13 +160,20 @@ export async function activity(previous: ActivityPayload = {}) {
 	const swapBigAndSmallImage = config[CONFIG_KEYS.SwapBigAndSmallImage];
 
 	const appName = env.appName;
-	const defaultSmallImageKey = debug.activeDebugSession
+	const smallImageKey = debug.activeDebugSession
 		? DEBUG_IMAGE_KEY
 		: appName.includes('Insiders')
 		? VSCODE_INSIDERS_IMAGE_KEY
 		: VSCODE_IMAGE_KEY;
-	const defaultSmallImageText = config[CONFIG_KEYS.SmallImage].replace(REPLACE_KEYS.AppName, appName);
-	const defaultLargeImageText = config[CONFIG_KEYS.LargeImageIdling];
+	const smallImageText = config[CONFIG_KEYS.SmallImage].replace(REPLACE_KEYS.AppName, appName);
+	const largeImageKey = window.activeTextEditor ? resolveFileIcon(window.activeTextEditor.document) : IDLE_IMAGE_KEY;
+	const largeImageText = window.activeTextEditor
+		? config[CONFIG_KEYS.LargeImage]
+				.replace(REPLACE_KEYS.LanguageLowerCase, toLower(largeImageKey))
+				.replace(REPLACE_KEYS.LanguageTitleCase, toTitle(largeImageKey))
+				.replace(REPLACE_KEYS.LanguageUpperCase, toUpper(largeImageKey))
+				.padEnd(2, FAKE_EMPTY)
+		: config[CONFIG_KEYS.LargeImageIdling];
 	const removeDetails = config[CONFIG_KEYS.RemoveDetails];
 	const removeLowerDetails = config[CONFIG_KEYS.RemoveLowerDetails];
 	const removeRemoteRepository = config[CONFIG_KEYS.RemoveRemoteRepository];
@@ -186,77 +182,46 @@ export async function activity(previous: ActivityPayload = {}) {
 		details: removeDetails
 			? undefined
 			: await details(CONFIG_KEYS.DetailsIdling, CONFIG_KEYS.DetailsEditing, CONFIG_KEYS.DetailsDebugging),
-		startTimestamp: config[CONFIG_KEYS.RemoveTimestamp] ? undefined : previous.startTimestamp ?? Date.now(),
-		largeImageKey: IDLE_IMAGE_KEY,
-		largeImageText: defaultLargeImageText,
-		smallImageKey: defaultSmallImageKey,
-		smallImageText: defaultSmallImageText,
-	};
-
-	if (swapBigAndSmallImage) {
-		state = {
-			...state,
-			largeImageKey: defaultSmallImageKey,
-			largeImageText: defaultSmallImageText,
-			smallImageKey: IDLE_IMAGE_KEY,
-			smallImageText: defaultLargeImageText,
-		};
-	}
-
-	const git = await getGit();
-	const repo = git?.repositories.find((repo) => repo.ui.selected);
-
-	if (!removeRemoteRepository && repo) {
-		const repoURL = repo.state.remotes[0].fetchUrl;
-		const repo_data = repoURL ? GitUrlParse(repoURL) : undefined;
-
-		if (repo_data) {
-			const viewRemoteRepository = config[CONFIG_KEYS.ViewRemoteRepository];
-			const viewRemoteRepositoryText = await repoDetails(viewRemoteRepository);
-
-			state = {
-				...state,
-				buttons: [{ label: viewRemoteRepositoryText, url: repo_data.href }],
-			};
-		}
-	}
-
-	if (window.activeTextEditor) {
-		const largeImageKey = resolveFileIcon(window.activeTextEditor.document);
-		const largeImageText = config[CONFIG_KEYS.LargeImage]
-			.replace(REPLACE_KEYS.LanguageLowerCase, toLower(largeImageKey))
-			.replace(REPLACE_KEYS.LanguageTitleCase, toTitle(largeImageKey))
-			.replace(REPLACE_KEYS.LanguageUpperCase, toUpper(largeImageKey))
-			.padEnd(2, FAKE_EMPTY);
-
-		state = {
-			...state,
-			details: removeDetails
-				? undefined
-				: await details(CONFIG_KEYS.DetailsIdling, CONFIG_KEYS.DetailsEditing, CONFIG_KEYS.DetailsDebugging),
-			state: removeLowerDetails
+		state:
+			window.activeTextEditor && removeLowerDetails
 				? undefined
 				: await details(
 						CONFIG_KEYS.LowerDetailsIdling,
 						CONFIG_KEYS.LowerDetailsEditing,
 						CONFIG_KEYS.LowerDetailsDebugging,
 				  ),
+		startTimestamp: config[CONFIG_KEYS.RemoveTimestamp] ? undefined : previous.startTimestamp ?? Date.now(),
+		largeImageKey,
+		largeImageText,
+		smallImageKey,
+		smallImageText,
+	};
+
+	if (swapBigAndSmallImage) {
+		[state.smallImageKey, state.largeImageKey, state.smallImageText, state.largeImageText] = [
+			state.largeImageKey,
+			state.smallImageKey,
+			state.largeImageText,
+			state.smallImageText,
+		];
+	}
+
+	const git = await getGit();
+	const repo = git?.repositories.find((repo) => repo.ui.selected);
+	const repoURL = repo?.state.remotes[0].fetchUrl;
+	const repo_data = repoURL ? GitUrlParse(repoURL) : undefined;
+
+	if (!removeRemoteRepository && repo_data?.href) {
+		let viewRemoteRepository = config[CONFIG_KEYS.ViewRemoteRepository];
+		viewRemoteRepository = await catchDetailsErrors(repoDetails, 'repository', viewRemoteRepository);
+
+		state = {
+			...state,
+			buttons: [{ label: viewRemoteRepository, url: repo_data.href }],
 		};
+	}
 
-		if (swapBigAndSmallImage) {
-			state = {
-				...state,
-				smallImageKey: largeImageKey,
-				smallImageText: largeImageText,
-			};
-		} else {
-			state = {
-				...state,
-				largeImageKey,
-				largeImageText,
-			};
-		}
-
+	if (window.activeTextEditor) {
 		log(LogLevel.Trace, `VSCode language id: ${window.activeTextEditor.document.languageId}`);
 	}
 
