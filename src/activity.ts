b078f225379d59b2a8029641b85c8b7d947fe29c
Espen Jacobsson <espenjacobsson@gmail.com>
2022-09-05 18:24:00 +0200

    Fix activity.ts

--- src/activity.ts
+++ src/activity.ts
@@ -110,6 +110,7 @@ async function details(idling: CONFIG_KEYS, editing: CONFIG_KEYS, debugging: CON
 
 	if (window.activeTextEditor) {
 		const fileName = basename(window.activeTextEditor.document.fileName);
+		const fileExtension = extname(window.activeTextEditor.document.fileName).replace('.', '');
 		const { dir } = parse(window.activeTextEditor.document.fileName);
 		const split = dir.split(sep);
 		const dirName = split[split.length - 1];
@@ -144,6 +145,7 @@ async function details(idling: CONFIG_KEYS, editing: CONFIG_KEYS, debugging: CON
 		}
 		raw = raw
 			.replace(REPLACE_KEYS.FileName, fileName)
+			.replace(REPLACE_KEYS.FileExtension, fileExtension)
 			.replace(REPLACE_KEYS.DirName, dirName)
 			.replace(REPLACE_KEYS.Workspace, workspaceName)
 			.replace(REPLACE_KEYS.WorkspaceFolder, workspaceFolderName)
@@ -252,127 +254,4 @@ export async function activity(previous: ActivityPayload = {}) {
 	}
 
 	return state;
-}
-
-async function details(idling: CONFIG_KEYS, editing: CONFIG_KEYS, debugging: CONFIG_KEYS) {
-	const config = getConfig();
-	let raw = (config[idling] as string).replace(REPLACE_KEYS.Empty, FAKE_EMPTY);
-
-	if (window.activeTextEditor) {
-		const fileName = basename(window.activeTextEditor.document.fileName);
-		const fileExtension = extname(window.activeTextEditor.document.fileName).replace('.', '');
-		const { dir } = parse(window.activeTextEditor.document.fileName);
-		const split = dir.split(sep);
-		const dirName = split[split.length - 1];
-
-		const noWorkspaceFound = config[CONFIG_KEYS.LowerDetailsNoWorkspaceFound].replace(REPLACE_KEYS.Empty, FAKE_EMPTY);
-		const workspaceFolder = workspace.getWorkspaceFolder(window.activeTextEditor.document.uri);
-		const workspaceFolderName = workspaceFolder?.name ?? noWorkspaceFound;
-		const workspaceName = workspace.name?.replace(REPLACE_KEYS.VSCodeWorkspace, EMPTY) ?? workspaceFolderName;
-		const workspaceAndFolder = `${workspaceName}${
-			workspaceFolderName === FAKE_EMPTY ? '' : ` - ${workspaceFolderName}`
-		}`;
-
-		const fileIcon = resolveFileIcon(window.activeTextEditor.document);
-
-		if (debug.activeDebugSession) {
-			raw = config[debugging] as string;
-		} else {
-			raw = config[editing] as string;
-		}
-
-		if (workspaceFolder) {
-			const { name } = workspaceFolder;
-			const relativePath = workspace.asRelativePath(window.activeTextEditor.document.fileName).split(sep);
-			relativePath.splice(-1, 1);
-			raw = raw.replace(REPLACE_KEYS.FullDirName, `${name}${sep}${relativePath.join(sep)}`);
-		}
-
-		try {
-			raw = await fileDetails(raw, window.activeTextEditor.document, window.activeTextEditor.selection);
-		} catch (error) {
-			log(LogLevel.Error, `Failed to generate file details: ${error as string}`);
-		}
-		raw = raw
-			.replace(REPLACE_KEYS.FileName, fileName)
-			.replace(REPLACE_KEYS.FileExtension, fileExtension)
-			.replace(REPLACE_KEYS.DirName, dirName)
-			.replace(REPLACE_KEYS.Workspace, workspaceName)
-			.replace(REPLACE_KEYS.WorkspaceFolder, workspaceFolderName)
-			.replace(REPLACE_KEYS.WorkspaceAndFolder, workspaceAndFolder)
-			.replace(REPLACE_KEYS.LanguageLowerCase, toLower(fileIcon))
-			.replace(REPLACE_KEYS.LanguageTitleCase, toTitle(fileIcon))
-			.replace(REPLACE_KEYS.LanguageUpperCase, toUpper(fileIcon));
-	}
-
-	return raw;
-}
-
-async function fileDetails(_raw: string, document: TextDocument, selection: Selection) {
-	let raw = _raw.slice();
-
-	if (raw.includes(REPLACE_KEYS.TotalLines)) {
-		raw = raw.replace(REPLACE_KEYS.TotalLines, document.lineCount.toLocaleString());
-	}
-
-	if (raw.includes(REPLACE_KEYS.CurrentLine)) {
-		raw = raw.replace(REPLACE_KEYS.CurrentLine, (selection.active.line + 1).toLocaleString());
-	}
-
-	if (raw.includes(REPLACE_KEYS.CurrentColumn)) {
-		raw = raw.replace(REPLACE_KEYS.CurrentColumn, (selection.active.character + 1).toLocaleString());
-	}
-
-	if (raw.includes(REPLACE_KEYS.FileSize)) {
-		let currentDivision = 0;
-		let size: number;
-		try {
-			({ size } = await workspace.fs.stat(document.uri));
-		} catch {
-			size = document.getText().length;
-		}
-		const originalSize = size;
-		if (originalSize > 1000) {
-			size /= 1000;
-			currentDivision++;
-			while (size > 1000) {
-				currentDivision++;
-				size /= 1000;
-			}
-		}
-
-		raw = raw.replace(
-			REPLACE_KEYS.FileSize,
-			`${originalSize > 1000 ? size.toFixed(2) : size}${FILE_SIZES[currentDivision]}`,
-		);
-	}
-
-	const git = await getGit();
-
-	if (raw.includes(REPLACE_KEYS.GitBranch)) {
-		if (git?.repositories.length) {
-			raw = raw.replace(
-				REPLACE_KEYS.GitBranch,
-				git.repositories.find((repo) => repo.ui.selected)?.state.HEAD?.name ?? FAKE_EMPTY,
-			);
-		} else {
-			raw = raw.replace(REPLACE_KEYS.GitBranch, UNKNOWN_GIT_BRANCH);
-		}
-	}
-
-	if (raw.includes(REPLACE_KEYS.GitRepoName)) {
-		if (git?.repositories.length) {
-			raw = raw.replace(
-				REPLACE_KEYS.GitRepoName,
-				git.repositories
-					.find((repo) => repo.ui.selected)
-					?.state.remotes[0].fetchUrl?.split('/')[1]
-					.replace('.git', '') ?? FAKE_EMPTY,
-			);
-		} else {
-			raw = raw.replace(REPLACE_KEYS.GitRepoName, UNKNOWN_GIT_REPO_NAME);
-		}
-	}
-
-	return raw;
 }
